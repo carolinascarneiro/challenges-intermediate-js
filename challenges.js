f774081ab37e2182edf8f4ad0cb525d869d317bc
Carolina Carneiro <cc@itacs.de>
2022-07-27 15:32:07 +0200

    committing challenges

--- challenges.js
+++ challenges.js
@@ -24,3 +24,29 @@ const reverseArray = (array) => {
     }
     return array.slice(array.length/2);
 }
+
+// Write a function, greetAliens(), that takes in an array of strings and uses a for loop to print a greeting with each string in the array.
+
+// The greeting should take the following format:
+// “Oh powerful [stringElement], we humans offer our unconditional surrender!”
+
+const greetAliens = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      console.log(`Oh powerful ${array[i]}, we humans offer our unconditional surrender!`);
+    }
+  }
+
+// Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '.
+
+// const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+ 
+// convertToBaby(animals); 
+// Should return ['baby panda', 'baby turtle', 'baby giraffe', 'baby hippo', 'baby sloth', 'baby human'];
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+const convertToBaby = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = 'baby ' + array[i];
+  }
+  return array;
+}
