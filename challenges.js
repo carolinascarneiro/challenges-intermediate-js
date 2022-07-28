517ee1902b56ca3d727f0e0a78e472a70e7e70bb
Carolina Carneiro <carneiro.carolina@yahoo.com.br>
2022-07-28 10:01:11 +0200

    committing challenges

--- challenges.js
+++ challenges.js
@@ -158,4 +158,18 @@ const shoutGreetings = array => {
   
   console.log(shoutGreetings(greetings))
   // Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
+
+//   Write a function sortYears() that takes in an array of years, and, using the built-in .sort() method, returns that array with the years sorted in descending order.
+
+// You can test your function when you’re ready by passing in the years array or by making your own array of years!
+
+// Write your code here:
+const sortYears = array => array.sort().reverse();
+
+// Feel free to uncomment the below code to test your function:
+/**/
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+
+console.log(sortYears(years))
+// Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
   
