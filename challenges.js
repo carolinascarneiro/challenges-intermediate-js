b308599158dbcd4d122e2890030f3fcb70a76e99
Carolina Carneiro <carneiro.carolina@yahoo.com.br>
2022-07-28 10:25:08 +0200

    committing challenges

--- challenges.js
+++ challenges.js
@@ -172,4 +172,27 @@ const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
 
 console.log(sortYears(years))
 // Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
+
+// .
+// Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays.
+
+// let arr1 = ['this', 'not this', 'nor this'];
+// let arr2 = ['thing 1', 'thing 2', 'this'];
+ 
+// justCoolStuff(arr1, arr2); // Should return ['this']
+// You can test your function when you’re ready by passing in the myStuff and coolStuff arrays or by making arrays of your own!
+
+// Write your code here:
+const justCoolStuff = (coolStuff, myStuff) => {
+    return coolStuff.filter(word => myStuff.includes(word));
+  }
+  
+  // Feel free to uncomment the code below to test your function
+  /**/
+  const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+  
+  const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+  
+  console.log(justCoolStuff(myStuff, coolStuff))
+  // Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
   
