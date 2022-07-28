d47f5b62c68d5ec08a5d4695dc3da79cc80fd141
Carolina Carneiro <cc@itacs.de>
2022-07-28 09:26:32 +0200

    committing challenges

--- challenges.js
+++ challenges.js
@@ -108,3 +108,23 @@ const acceptOne = (veg) => {
 const acceptEverything = (array) => {
       array.forEach(acceptOne);
 }
+
+// Write a function, squareNums(), that takes in an array of numbers and, using .map(), returns an array with the square of each of the elements of that array.
+
+// You can test your function when you’re ready by passing in the numbers array or by making your own array!
+
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+
+const squareNums = array => {
+
+const map1 = array.map(toSquare);
+
+return map1;
+
+}
+
+console.log(squareNums(numbers))
